245045bf6ffc2b73364ac0600b95207f7fe99728
ajuanchuto <ajuanchuto@uc.cl>
2021-09-21 22:11:27 -0300

    test(cell_test): add cell tests for the cell class

---
+++ test/cell_test.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require_relative 'test_helper'
+require_relative '../lib/cell'
+require 'test/unit'
+
+class CellTest < Test::Unit::TestCase
+  def setup
+    @cell = Cell.new(0)
+  end
+
+  def test_delete_flag
+    @cell.put_flag
+    @cell.delete_flag
+    assert_false(@cell.flag)
+  end
+
+  def test_put_flag
+    @cell.put_flag
+    assert_true(@cell.flag)
+  end
+
+  def test_uncover_cell
+    value = @cell.uncover_cell
+    assert_true(@cell.visible)
+    assert_equal(0, value)
+  end
+end
