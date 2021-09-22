a1443dd558d932bbccb8cd30c61ac5f4d7ca6087
ajuanchuto <ajuanchuto@uc.cl>
2021-09-21 22:10:34 -0300

    feat(cell): add cell class to lib

---
+++ lib/cell.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# Class to store the information in each cell on the board
+class Cell
+  attr_reader :flag, :value, :visible
+
+  def initialize(value)
+    @flag = false
+    @value = value
+    @visible = false
+  end
+
+  def put_flag
+    @flag = true
+  end
+
+  def delete_flag
+    @flag = false
+  end
+
+  def uncover_cell
+    @visible = true
+    @value
+  end
+end
