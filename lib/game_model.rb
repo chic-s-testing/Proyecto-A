0c440e12d649dbd9af9b12d9453a84945632476c
Javiera Ochoa <jiochoa@uc.cl>
2021-09-27 16:46:12 -0300

    feat(check_cell_bomb): method created

--- lib/game_model.rb
+++ lib/game_model.rb
@@ -13,6 +13,13 @@ class GameModel
     @number_not_bombs = @board_instance.not_bombs_cells
     @number_discovered = 0
   end
+
+  def check_cell_bomb(pos_i, pos_j)
+    cell = @board[pos_i][pos_j]
+    return true if cell.value == -1
+
+    false
+  end
 end
 
 GameModel.new
