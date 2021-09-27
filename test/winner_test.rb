b026e8d5c86f519f38f46f744d174031012c8b0d
barbaraim <birarrazaval1@uc.cl>
2021-09-27 20:47:03 -0300

    test(model) tests for not winner boards

--- test/winner_test.rb
+++ test/winner_test.rb
@@ -9,6 +9,21 @@ class InitializeModelTest < Test::Unit::TestCase
     @model = GameModel.new
   end
 
+  def not_winner_first_cell_test
+    cells_discovered = 0
+    @model.board.each do |row|
+      row.each do |cell|
+        cell.uncover if cell.value >= 0 && cells_discovered.zero?
+      end
+    end
+    assert_false(@model.winner)
+  end
+
+  def not_winner_random_cell_test
+    @model.board[rand(0..9)][rand(0..9)] if @model.board[rand(0..9)][rand(0..9)].value >= 0
+    assert_false(@model.winner)
+  end
+
   def winner_test
     @model.board.each do |row|
       row.each do |cell|
