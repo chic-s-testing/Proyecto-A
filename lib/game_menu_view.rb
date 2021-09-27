54d2c6d34083f98441060cdac1b5d6d5c1cc2bd5
ajuanchuto <ajuanchuto@uc.cl>
2021-09-27 18:31:13 -0300

    feat(lib/game_menu_view.rb): create method print_initial_game

--- lib/game_menu_view.rb
+++ lib/game_menu_view.rb
@@ -2,16 +2,22 @@
 
 # Game's menu view
 class GameMenuView
-    def puts_start_menu()
-        puts "Menu Minesweeper"
-    end
+  def puts_start_menu
+    puts 'Menu Minesweeper'
+  end
 
-    def puts_request_coordinate(coordinate)
-        puts "Write the coordinate #{coordinate}:"
-    end
+  def puts_request_coordinate(coordinate)
+    puts "Write the coordinate #{coordinate}:"
+  end
 
-    def puts_request_action()
-        puts "Please Write the action you want to do (put flag, delete flag, uncover cell):"
-    end
+  def puts_request_action
+    puts 'Please Write the action you want to do (put flag, delete flag, uncover cell):'
+  end
+
+  def print_initial_game
+    puts '¡Welcome to Minesweeper!'
+    puts 'The objective of the game is to clear a minefield without detonating any mine.'
+    puts 'You can also put flags where you think there are mines...'
+    puts 'Good Luck :)'
+  end
 end
-  
