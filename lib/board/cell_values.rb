3ebd271be1a5d1c6e3753a5f57b64d479a5fe4b6
barbaraim <birarrazaval1@uc.cl>
2021-09-26 22:36:23 -0300

    fix(cell_values/model) fix methods needed for create value for rubocop

--- lib/board/cell_values.rb
+++ lib/board/cell_values.rb
@@ -1,23 +1,16 @@
 # frozen_string_literal: true
 
 def check_around_values(board, row, column)
+  spaces_to_check = [[1, 1], [1, -1], [-1, -1], [-1, 1], [1, 0], [0, 1], [-1, 0], [0, -1]]
   bombs = 0
-  spaces_to_check = space_list(row, column)
-  spaces_to_check.each do |space|
-    bombs += 1 if !board[row + space[0]][column + space[1]].nil? && board[row + space[0]][column + space[1]].value == -1
+  spaces_to_check.each do |coordinate|
+    row_to_check = row + coordinate[0]
+    col_to_check = column + coordinate[1]
+    bombs += 1 if in_board(row_to_check, col_to_check) && board[row_to_check][col_to_check].value == -1
   end
   bombs
 end
 
-def space_list(row, column)
-  spaces_to_check = []
-  spaces_to_check.append([-1, -1]) if row.positive? && column.positive?
-  spaces_to_check.append([0, -1]) if column.positive?
-  spaces_to_check.append([-1, 0]) if row.positive?
-  spaces_to_check.append([-1, 1]) if column < 9 && row.positive?
-  spaces_to_check.append([0, 1]) if column < 9
-  spaces_to_check.append([1, 1]) if column < 9 && row < 9
-  spaces_to_check.append([1, 0]) if row < 9
-  spaces_to_check.append([1, -1]) if column.positive? && row < 9
-  spaces_to_check
+def in_board(row_to_check, col_to_check)
+  (row_to_check) <= 9 && (row_to_check) >= 0 && (col_to_check) <= 9 && (col_to_check) >= 0
 end
