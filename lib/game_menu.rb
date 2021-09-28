82b5afd57e0647538c301071eb5f181f43a87746
Dnpoblete <dnpoblete@uc.cl>
2021-09-27 21:43:56 -0300

    fix(test and lib): Fixing Rubocop conflicts

--- lib/game_menu.rb
+++ lib/game_menu.rb
@@ -4,25 +4,22 @@ require_relative './game_menu_view'
 
 # Game's menu
 class GameMenu
+  def initialize
+    @game_view = GameMenuView.new
+  end
 
-    def initialize()
-        @game_view = GameMenuView.new
-    end
+  def request_x_coordinate
+    @game_view.puts_request_coordinate('X')
+    $stdin.gets.to_i
+  end
 
-    def request_x_coordinate()
-        @game_view.puts_request_coordinate("X")
-        x = $stdin.gets.to_i
-    end
+  def request_y_coordinate
+    @game_view.puts_request_coordinate('Y')
+    $stdin.gets.to_i
+  end
 
-    def request_y_coordinate()
-        @game_view.puts_request_coordinate("Y")
-        y = $stdin.gets.to_i
-    end
-
-    def request_action()
-        @game_view.puts_request_action()
-        action = $stdin.gets.to_s
-        return action
-    end
+  def request_action
+    @game_view.puts_request_action
+    $stdin.gets.to_s
+  end
 end
-  
--- test/game_menu_test.rb
+++ test/game_menu_test.rb
@@ -6,24 +6,22 @@ require 'test/unit'
 require_relative '../lib/game_menu'
 
 class GameMenuTest < Test::Unit::TestCase
-
   def setup
     @menu = GameMenu.new
   end
 
   def test_request_x_coordinate
-    $stdin = StringIO.new("10")
-    assert_equal(@menu.request_x_coordinate(), 10)
+    $stdin = StringIO.new('10')
+    assert_equal(@menu.request_x_coordinate, 10)
   end
 
   def test_request_y_coordinate
-    $stdin = StringIO.new("1")
-    assert_equal(@menu.request_y_coordinate(), 1)
+    $stdin = StringIO.new('1')
+    assert_equal(@menu.request_y_coordinate, 1)
   end
 
   def test_request_action
-    $stdin = StringIO.new("put flag")
-    assert_equal(@menu.request_action(), "put flag")
+    $stdin = StringIO.new('put flag')
+    assert_equal(@menu.request_action, 'put flag')
   end
-
 end
