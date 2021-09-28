9c81e5e7ab0451f2825f4384dd04633c4c3ce770
Javiera Ochoa <jiochoa@uc.cl>
2021-09-27 21:12:59 -0300

    fix(controller): menu_view added as parameter

--- lib/game_controller.rb
+++ lib/game_controller.rb
@@ -2,8 +2,9 @@
 
 # Game's controller
 class GameController
-  def initialize(board_view, board_model)
+  def initialize(board_view, board_model, menu_view)
     @model = board_model
     @view = board_view
+    @menu_view = menu_view
   end
 end
