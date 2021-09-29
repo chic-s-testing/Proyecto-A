06f0f9ba0aa10684b499ba6a4b111ce945ca22b1
Rodrigo Alonso <rialonso@uc.cl>
2021-09-29 11:39:15 -0300

    fix(controller): fix errors when calling methods

--- lib/game_controller.rb
+++ lib/game_controller.rb
@@ -11,27 +11,27 @@ class GameController
   def select_coordinates(row, col, action = 'uncover')
     case action
     when 'uncover'
-      return 'game_over' if check_game_over(row, col)
+      return 'game_over' if check_game_over_routine(row, col)
 
       @model.mark_uncover(row, col)
-      return 'winner' if check_game_over_routine(row, col)
+      return 'winner' if check_winner_routine
     when 'put_flag' then @model.mark_add_flag(row, col)
     when 'remove_flag' then @model.mark_remove_flag(row, col)
     end
     'next'
   end
 
-  def check_winner_routine(row, col)
-    if @view.check_cell_bomb(row, col)
-      @view.puts_game_over
+  def check_winner_routine
+    if @model.winner
+      @view.puts_congratulation
       return true
     end
     false
   end
 
-  def check_game_over_routine(_row, _col)
-    if @model.winner
-      @view.puts_congratulation
+  def check_game_over_routine(row, col)
+    if @model.check_cell_bomb(row, col)
+      @view.puts_game_over
       return true
     end
     false
