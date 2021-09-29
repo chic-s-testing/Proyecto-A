3eaf736bf0ed17d4d1963198bf87f32c0879f7bb
Rodrigo Alonso <rialonso@uc.cl>
2021-09-29 20:58:06 -0300

    fix(winner-test): fix winner_test function names

--- test/winner_test.rb
+++ test/winner_test.rb
@@ -6,28 +6,28 @@ require 'test/unit'
 
 class ModelWinnerTest < Test::Unit::TestCase
   def setup
-    @model = GameModel.new
+    @model = GameModel.new()
   end
 
-  def not_winner_first_cell_test
+  def test_not_winner_first_cell
     cells_discovered = 0
     @model.board.each do |row|
       row.each do |cell|
-        cell.uncover if cell.value >= 0 && cells_discovered.zero?
+        cell.uncover_cell if cell.value >= 0 && cells_discovered.zero?
       end
     end
     assert_false(@model.winner)
   end
 
-  def not_winner_random_cell_test
+  def test_not_winner_random_cell
     @model.board[rand(0..9)][rand(0..9)] if @model.board[rand(0..9)][rand(0..9)].value >= 0
     assert_false(@model.winner)
   end
 
-  def winner_test
-    @model.board.each do |row|
-      row.each do |cell|
-        cell.uncover if cell.value >= 0
+  def test_winner
+    (0..9).each do | row |
+      (0..9).each do | col | 
+        @model.mark_uncover(row, col) if @model.board[row][col].value >= 0
       end
     end
     assert_true(@model.winner)
