e698c4721b4e821ab8dc9f297214d2eb7453da99
Javiera Ochoa <jiochoa@uc.cl>
2021-09-29 20:09:40 -0300

    fix(menu-view): delete menu-view duplicated instance

--- lib/main.rb
+++ lib/main.rb
@@ -6,11 +6,13 @@ require_relative './game_model'
 require_relative './game_controller'
 require_relative './game_view'
 require_relative './game_menu_view'
+require_relative './game_menu'
 
 model = GameModel.new
 view = GameView.new
 menu_view = GameMenuView.new
 model.addObserver(view)
+game_menu = GameMenu.new(menu_view)
 controller = BoardController.new(model, view, menu_view)
 
 menu_view.print_initial_game
