46bbe74bd493dda98830be6c244d2737429595b6
Javiera Ochoa <jiochoa@uc.cl>
2021-10-01 19:48:53 -0300

    test(observer): add test for update error

--- test/observer_test.rb
+++ test/observer_test.rb
@@ -15,4 +15,9 @@ class ObserverTest < Test::Unit::TestCase
     @observable.add_observer(@observer)
     assert_equal(@observable.observers[0], @observer)
   end
+
+  def test_update_error
+    board = []
+    assert_raise(RuntimeError.new('Cannot initialize an abstract method')) { @observer.update(board) }
+  end
 end
